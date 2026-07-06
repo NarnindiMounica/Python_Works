06cf069780180ca905f07ea60e9973576e789101
NarnindiMounica <mounica.narnindi12@gmail.com>
2026-07-07 01:31:37 +0530

    insertion sorting

---
+++ Assignments/insertion_sort.py
@@ -0,0 +1,19 @@
+def insertion_sorting(arr):
+    "this function is used for insertion sort"
+    num = len(arr)
+
+    for i in range(1, num):
+        current_card = arr[i]
+        current_pos = i-1 #this will go from i-1 to 0
+
+        while current_pos >= 0:
+            if arr[current_pos] < current_card:
+                break
+            arr[current_pos]+1 = arr[current_pos]
+            current_pos = current_pos -1
+
+    return arr
+
+
+print(insertion_sorting(arr=[23, 45, 67, 12, 34]))
+ 
