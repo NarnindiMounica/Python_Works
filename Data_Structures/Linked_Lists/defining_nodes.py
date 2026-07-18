a44ed678cc0ab6543cab074b087f130e5689485f
NarnindiMounica <mounica.narnindi12@gmail.com>
2026-07-18 17:37:05 +0530

    linked lists : defining nodes class

---
+++ Data_Structures/Linked_Lists/defining_nodes.py
@@ -0,0 +1,11 @@
+class Node:
+    def __init__(self, value):
+        self.data = value
+        self.next = None
+
+first = Node(1)
+second = Node(2) 
+
+print(id(first), id(second))
+
+first.next = second
