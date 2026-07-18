9582bac81fda4135bb5a0cd0c027f2afce8baaba
NarnindiMounica <mounica.narnindi12@gmail.com>
2026-07-18 22:47:45 +0530

    linked lists : insertion at head of linked list

---
+++ Data_Structures/Linked_Lists/inserting_at_head.py
@@ -0,0 +1,17 @@
+from commons import Node, take_input_better, print_ll
+
+head = take_input_better()
+print("Linked List Before Insertion")
+print_ll(head)
+
+def insert_at_head(head, data):
+    new_node = Node(value=data)
+
+    new_node.next = head
+    head = new_node
+
+    return head
+
+head_after_insertion = insert_at_head(head, data=12)
+print("Linked List After Insertion")
+print_ll(head_after_insertion)
