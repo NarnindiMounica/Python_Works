64bdd8713ee7be770ea4f4b5231f1c8217bd7c73
NarnindiMounica <mounica.narnindi12@gmail.com>
2026-07-19 18:19:16 +0530

    linked lists: deleting a node by value one first occurrence

--- Data_Structures/Linked_Lists/deleting_by_value.py
+++ Data_Structures/Linked_Lists/deleting_by_value.py
@@ -1,6 +1,6 @@
 from commons import print_ll, take_input_better
 
-def deleting_node_by_value_all_occurences(head, value):
+def deleting_node_by_value_all_occurrence(head, value):
 
     if head==None:  #empty linked list
         return None
@@ -16,8 +16,32 @@ def deleting_node_by_value_all_occurences(head, value):
 
     return head
 
-head = take_input_better()
-head_after_deletion = deleting_node_by_value_all_occurences(head, value=20)
-print_ll(head_after_deletion)
+# head = take_input_better()
+# head_after_deletion = deleting_node_by_value_all_occurrence(head, value=20)
+# print_ll(head_after_deletion)
     
+
+def deleting_node_by_value_first_occurrence(head, value):
+
+    if head == None:
+        return None
+    
+    if head.data == value:
+        return head.next
+    
+    temp = head
+    
+    while temp.next != None and temp.next.data != value:
+        temp = temp.next
+
+    if temp.next == None: # to handle case if value is not in given linked list  
+        return head  
+
+    temp.next = temp.next.next
+
+    return head  
+
+head = take_input_better()
+head_after_deletion = deleting_node_by_value_first_occurrence(head, value=20)
+print_ll(head_after_deletion)  
 
