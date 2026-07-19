57cbef20c13550461f9cca0228a308d9d5101e4c
NarnindiMounica <mounica.narnindi12@gmail.com>
2026-07-19 15:48:09 +0530

    linked list: deleting node at given index in linked list using recursion

--- Data_Structures/Linked_Lists/deleting_at_index.py
+++ Data_Structures/Linked_Lists/deleting_at_index.py
@@ -17,8 +17,25 @@ def delete_at_index(head, inx):
         
     return head
 
+#head = take_input_better()
+#head_after_deletion = delete_at_index(head, inx=100)
+#print_ll(head_after_deletion)
+
+
+def deleting_at_index_recursion(head, inx):
+
+    if head == None:
+        return None
+    
+    if inx == 0:
+        return head.next
+    
+    head.next = deleting_at_index_recursion(head.next, inx-1)
+
+    return head
+
 head = take_input_better()
-head_after_deletion = delete_at_index(head, inx=100)
-print_ll(head_after_deletion)
+head_after_deletion_recursion = deleting_at_index_recursion(head, inx=0)
+print_ll(head_after_deletion_recursion)
 
 
