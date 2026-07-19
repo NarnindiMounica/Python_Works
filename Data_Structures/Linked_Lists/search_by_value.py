8690b3e3194300d6431e94f04a4475368a08da71
NarnindiMounica <mounica.narnindi12@gmail.com>
2026-07-19 18:43:44 +0530

    linked lists: search element by value from linked list

--- Data_Structures/Linked_Lists/search_by_value.py
+++ Data_Structures/Linked_Lists/search_by_value.py
@@ -1,3 +1,22 @@
 from commons import print_ll, create_ll_from_list
 
-head = create_ll_from_list(list=)
+
+def search_by_value(head, value):
+    inx = 0
+    temp = head
+
+    if temp.data == value:
+        return inx
+    
+    while temp is not None:
+        if temp.data == value:
+            return inx
+        temp = temp.next
+        inx = inx+1
+
+    return -1    
+
+head = create_ll_from_list(lst=[1,2,3,4,5])
+print(search_by_value(head, value=3))
+
+
