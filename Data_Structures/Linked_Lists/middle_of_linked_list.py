4bee952aa49922690f9eb9af83719c804a2466ac
NarnindiMounica <mounica.narnindi12@gmail.com>
2026-07-22 01:48:55 +0530

    linked list: middle of linked list using slow-fast technique

--- Data_Structures/Linked_Lists/middle_of_linked_list.py
+++ Data_Structures/Linked_Lists/middle_of_linked_list.py
@@ -1,12 +1,15 @@
 from commons import Node, create_ll_from_list, print_ll
 
-head = create_ll_from_list([1,2,3,4,5,6,7,8])
+head = create_ll_from_list([1,2,3,4,5,6,7,8,9,10,11])
 print_ll(head)
 
 def middle_of_linked_list(head):
 
     len_of_ll = 0
     temp = head
+
+    if head==None or head.next == None:
+        return head
 
     while temp.next != None:
         temp = temp.next
@@ -21,4 +24,23 @@ def middle_of_linked_list(head):
         count += 1
     return temp.data
 print()
-print(middle_of_linked_list(head))        
+#print(middle_of_linked_list(head))        
+
+
+def middle_of_ll_better(head):
+    
+    if head==None or head.next == None:
+        return head
+    
+    slow = head
+    fast = head
+     
+    while fast is not None and fast.next is not None:
+        slow = slow.next
+        fast = fast.next.next
+    return slow.data 
+
+print(middle_of_ll_better(head))
+        
+
+
