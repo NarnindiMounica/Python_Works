2f494e48e8d5eee6ac1f96ae1a31f18a434f13be
NarnindiMounica <mounica.narnindi12@gmail.com>
2026-07-22 00:50:42 +0530

    linked list: middle of linked list

--- Data_Structures/Linked_Lists/middle_of_linked_list.py
+++ Data_Structures/Linked_Lists/middle_of_linked_list.py
@@ -1,8 +1,24 @@
 from commons import Node, create_ll_from_list, print_ll
 
-head = create_ll_from_list([1,2,3,4,5])
+head = create_ll_from_list([1,2,3,4,5,6,7,8])
 print_ll(head)
 
 def middle_of_linked_list(head):
 
     len_of_ll = 0
+    temp = head
+
+    while temp.next != None:
+        temp = temp.next
+        len_of_ll += 1
+
+    middle = len_of_ll//2
+    
+    temp = head
+    count = 0
+    while count < middle:
+        temp = temp.next
+        count += 1
+    return temp.data
+print()
+print(middle_of_linked_list(head))        
