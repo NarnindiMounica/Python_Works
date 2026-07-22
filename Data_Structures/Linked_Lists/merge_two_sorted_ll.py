5b888001fb6de9c26495b2dfaf8febd6b6e6486b
NarnindiMounica <mounica.narnindi12@gmail.com>
2026-07-22 23:30:19 +0530

    linked list: merge two sorted linked list

--- Data_Structures/Linked_Lists/merge_two_sorted_ll.py
+++ Data_Structures/Linked_Lists/merge_two_sorted_ll.py
@@ -2,3 +2,16 @@ from commons import Node, create_ll_from_list, print_ll
 
 head1 = create_ll_from_list([1, 3, 4, 6])
 head2 = create_ll_from_list([2, 5, 7, 8, 9, 10])
+
+def merge_sorted_ll(head1, head2):
+
+    if head1 == None:
+        return head2 
+    
+    if head2 == None:
+        return head1
+    
+    final_head = None
+    final_tail = None
+
+    while 
