484630b2d216a064f73b9ddc3728d532dab9574a
NarnindiMounica <mounica.narnindi12@gmail.com>
2026-07-22 23:54:42 +0530

    linked list: merge two sorted linked list

--- Data_Structures/Linked_Lists/merge_two_sorted_ll.py
+++ Data_Structures/Linked_Lists/merge_two_sorted_ll.py
@@ -14,4 +14,35 @@ def merge_sorted_ll(head1, head2):
     final_head = None
     final_tail = None
 
-    while 
+    while head1 is not None and head2 is not None:
+
+        if head1.data < head2.data:
+            if final_head == None:
+                final_head = head1
+                final_tail = head1
+            else:
+                final_tail.next = head1
+                final_tail = head1
+            head1 = head1.next
+        elif head2.data < head1.data:
+            if final_head == None:
+                final_head = head2
+                final_tail = head2
+            else:
+                final_tail.next = head2
+                final_tail = head2
+            head2 = head2.next 
+
+    if head1 is not None:
+        final_tail.next = head1
+
+    if head2 is not None:
+        final_tail.next = head2
+
+    return final_head
+
+
+merged_ll_head = merge_sorted_ll(head1 = head1, head2 = head2)
+print_ll(merged_ll_head)
+
+
