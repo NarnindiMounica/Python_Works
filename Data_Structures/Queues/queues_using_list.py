28d7c367f3aeb62de3e3f699e0716cbe7cab14ae
NarnindiMounica <mounica.narnindi12@gmail.com>
2026-07-25 22:15:32 +0530

    queues: queues using list class

---
+++ Data_Structures/Queues/queues_using_list.py
@@ -0,0 +1,27 @@
+class QueueFromList:
+    def __init__(self):
+        self._queue = []
+
+    def enqueue(self, value):
+        self._queue.append(value)
+        return f"{value} enqueued in queue"
+
+    def is_empty(self):
+        return len(self._queue)==0  
+
+    def get_size(self):
+        return len(self._queue)
+
+    def front(self):
+        if self.get_size == 0:
+            return "Queue is empty, cannot return front"
+        return self._queue[0]
+
+    def dequeue(self):
+        if self.get_size == 0:
+            return "Queue is empty, cannot dequeue from it"
+        return self._queue.pop(0)
+
+queue_from_list = QueueFromList()
+
+print(queue_from_list.is_empty())
