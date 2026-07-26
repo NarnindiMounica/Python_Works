09d77d04966898ce12f347287ad779374421f826
NarnindiMounica <mounica.narnindi12@gmail.com>
2026-07-26 18:14:39 +0530

    Trees: commons file

--- Data_Structures/Trees/commons.py
+++ Data_Structures/Trees/commons.py
@@ -3,8 +3,13 @@ class TreeNode:
         self.data = data
         self.children = []
 
-root = TreeNode(1)  
-child1 = root.children.append(2)
-child2 = root.children.append(3)     
+root = TreeNode(1) 
+child1 = TreeNode(2)
+child2 = TreeNode(3)
+child3 = TreeNode(4) 
 
-print(root.children)
+root.children.append(child1)
+root.children.append(child2)
+root.children.append(child3)
+  
+print(root.children[0].data)
