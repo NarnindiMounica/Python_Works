2a3a30fceb1c1ef427dba4d038d68fe7f4f68de1
NarnindiMounica <mounica.narnindi12@gmail.com>
2026-08-02 17:41:44 +0530

    hashmaps: insert and get funtions

--- Data_Structures/Hashmaps/open_addressing_insert_and_search.py
+++ Data_Structures/Hashmaps/open_addressing_insert_and_search.py
@@ -7,10 +7,27 @@ class Hashmap:
 
     def hash_function(self, key):
         return abs(hash(key)) % self.capacity
+
+    def rehash(self, initial_value):
+        return initial_value + 1
     
     def insert(self, key, value):
-
         hash_value = self.hash_function(key) 
         initial_index = hash_value
+
+        if self.slots[initial_index] is None:
+            self.slots[initial_index] = key
+            self.values[initial_index] = value
+        else:
+            #updat if key is alreday present
+            if self.slots[initial_index] == key:
+                self.values[initial_index] = value
+
+            else:
+                new_hash_value = self.rehash(initial_index)
+                 
+
+
+
 
            
