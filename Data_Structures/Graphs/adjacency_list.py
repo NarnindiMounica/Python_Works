8604374e47cfb34519980629393222e1d8b9c57f
NarnindiMounica <mounica.narnindi12@gmail.com>
2026-08-05 21:29:28 +0530

    graphs: graphs using adjacency_list

--- Data_Structures/Graphs/adjacency_list.py
+++ Data_Structures/Graphs/adjacency_list.py
@@ -2,3 +2,11 @@ class GraphAdjacencyList:
     def __init__(self):
         self.vertices = []
         self.adj_list = []
+
+    def add_vertices(self, vertex):
+        if vertex not in self.vertices:
+            self.vertices.append(vertex)
+        else:
+            return f"vertex {vertex} already exists"
+
+            
