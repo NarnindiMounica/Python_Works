55de8fe3179afebe3195e8cb5d031045a08e565d
NarnindiMounica <mounica.narnindi12@gmail.com>
2026-08-05 21:45:01 +0530

    graphs: graphs using adjacency_list

--- Data_Structures/Graphs/adjacency_list.py
+++ Data_Structures/Graphs/adjacency_list.py
@@ -15,7 +15,28 @@ class GraphAdjacencyList:
             self.adj_list[source].append((destination, weight))
             self.adj_list[destination].append((source, weight))
         else:
-            return "one or more vertices not found"    
+            return "one or more vertices not found" 
+
+    def display(self):
+        print("Vertices:")
+        for vertex in self.vertices:
+            print(f"Vertex: {vertex}")
+
+        for vertex in self.adj_list.keys():
+            print(f"{vertex} ---> {self.adj_list[vertex]}")          
+
+graph = GraphAdjacencyList()
+graph.add_vertices('A')
+graph.add_vertices('B')
+graph.add_vertices('C')
+graph.add_vertices('D')
+graph.add_vertices('E')
+
+graph.add_edges('A', 'B', 1 )
+graph.add_edges('A', 'C', 1 )
+graph.add_edges('B', 'D', 1 )
+
+graph.display()
 
 
             
