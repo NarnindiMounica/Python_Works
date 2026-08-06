2ee375a078b6b364ef6c3dd0897786f3e05a62b6
NarnindiMounica <mounica.narnindi12@gmail.com>
2026-08-06 22:54:15 +0530

    graphs: adjacency matrix class

---
+++ Data_Structures/Graphs/adjacency_matrix.py
@@ -0,0 +1,5 @@
+class GraphAdjacencyMatrix:
+    def __init__(self, num_vertices):
+        self.num_vertices = num_vertices
+        self.vertices = [None] * num_vertices
+        self.adj_matrix = [[None] * num_vertices for _ in range(num_vertices)]
