8497aeeca3be0ed654a23ea884ffeb71017d68da
NarnindiMounica <mounica.narnindi12@gmail.com>
2026-08-06 22:56:14 +0530

    graphs: adjacency matrix class

--- Data_Structures/Graphs/adjacency_matrix.py
+++ Data_Structures/Graphs/adjacency_matrix.py
@@ -3,3 +3,9 @@ class GraphAdjacencyMatrix:
         self.num_vertices = num_vertices
         self.vertices = [None] * num_vertices
         self.adj_matrix = [[None] * num_vertices for _ in range(num_vertices)]
+
+    def add_vertices(self, index, label):
+        if index >= 0 and index < self.num_vertices:  
+            self.vertices[index] = label
+        else:
+            return "Index OOB"    
