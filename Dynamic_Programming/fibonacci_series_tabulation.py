e448197d4cb7920fc2c6a42f1781e58f0586e13d
NarnindiMounica <mounica.narnindi12@gmail.com>
2026-08-08 23:46:23 +0530

    dynamic programming: fibonacci_series - tabulation

--- Dynamic_Programming/fibonacci_series_tabulation.py
+++ Dynamic_Programming/fibonacci_series_tabulation.py
@@ -2,4 +2,12 @@ def fib_series_bottom_up(number):
     dp = [0] * (number + 1)
 
     dp[0] = 0
-    dp [1] = 1
+    dp[1] = 1
+
+    for i in range(2, number + 1):
+        dp[i] = dp[i-1] + dp[i-2]
+
+    return dp[number]
+
+number = 8
+print(fib_series_bottom_up(number))    
