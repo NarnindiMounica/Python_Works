dd9e33920110cc6cdfa302cac5fc9eff47a24705
NarnindiMounica <mounica.narnindi12@gmail.com>
2026-08-08 23:25:16 +0530

    dynamic programming: fibonacci_series

--- Dynamic_Programming/fibonacci_series.py
+++ Dynamic_Programming/fibonacci_series.py
@@ -26,13 +26,17 @@ def fib_series(number):
         return 1
     else:
         return fib_series(number-1) + fib_series(number-2)
+    
 
-dairy_num = [-1] * (9)
-def fib_series_with_dp(number):
+dairy_num = [-1] * (51)
+calls = {}
+def fib_series_with_dp(number):  
 
     if dairy_num[number] != -1:
         return dairy_num[number]
 
+    calls[number] = 1
+    
     if number == 0:
         return 0
     elif number == 1:
@@ -41,11 +45,12 @@ def fib_series_with_dp(number):
         dairy_num[number-1] = fib_series_with_dp(number-1)
         dairy_num[number-2] = fib_series_with_dp(number-2)
 
-        dairy_num[number] = dairy_num[number-1] + dairy_num[number-2]
+        dairy_num[number] = dairy_num[number-1] + dairy_num[number-2]       
 
         return dairy_num[number]
 
-print(fib_series_with_dp(8))
+print(fib_series_with_dp(50))
+print(calls)
 
 
 # print(fib_series_with_calls(5))
