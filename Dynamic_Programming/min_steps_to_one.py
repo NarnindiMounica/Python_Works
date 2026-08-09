8800ab5b35067afd586dd36f636b10f462cd5e96
NarnindiMounica <mounica.narnindi12@gmail.com>
2026-08-09 17:34:58 +0530

    dynamic programming: min steps to reach one

--- Dynamic_Programming/min_steps_to_one.py
+++ Dynamic_Programming/min_steps_to_one.py
@@ -22,18 +22,23 @@ def min_steps_to_one_dp(number, memo):
 
     if number == 1:
         return 0
+
+    if memo[number] != -1:
+        return memo[number]
+     
     print(f"{number} n-1 step")
-    steps = min_steps_to_one(number-1)+1
+    steps = min_steps_to_one_dp(number-1)+1
 
     print(f"{number} n//2 step")
     if number % 2 == 0:
-        steps = min(steps, min_steps_to_one(number//2))+1
+        steps = min(steps, min_steps_to_one_dp(number//2))+1
 
     print(f"{number} n//3 step")
     if number % 3 == 0:
-        steps = min(steps, min_steps_to_one(number//3))+1
+        steps = min(steps, min_steps_to_one_dp(number//3))+1
 
-    return steps    
+    memo[number] = steps
+    return memo[number]    
 
 number = 7
 memo = [-1] * (number + 1)
