1a32f616def771060a4a05fb7f9de0c17c34c53b
NarnindiMounica <mounica.narnindi12@gmail.com>
2026-08-12 00:55:11 +0530

    trees: max data node level wise

--- Data_Structures/Trees/max_data_node_level_wise.py
+++ Data_Structures/Trees/max_data_node_level_wise.py
@@ -1,11 +1,8 @@
 from collections import deque
 
 from generic_trees_input import predefined_generic_trees
+from commons import print_tree_detailed
 
-class Tree:
-    def __init__(self, val):
-        self.val = val
-        self.children = []
 
 def max_data_level_wise(root):
     if root == None:
@@ -13,13 +10,26 @@ def max_data_level_wise(root):
     
     queue = deque([root])
     max_node_list = []
-    while queue != 0:
-        current_node = queue.popleft()
-        temp = -1
-        for eachchild in current_node.children:
-            temp = max(temp, eachchild.val)
-            queue.append(eachchild)
-        max_node_list.append(temp)
+    
+    while len(queue) != 0:
+        level_size = len(queue)
+        temp = float('-inf')
+        for _ in range(level_size):
+            current_node = queue.popleft()
+            temp = max(temp, current_node.data)
+            for eachchild in current_node.children:
+                queue.append(eachchild)
+    max_node_list.append(temp)
+    return max_node_list
 
 
+root1, root2, root3 = predefined_generic_trees()
 
+print_tree_detailed(root1)
+print(max_data_level_wise(root1))
+print()
+print_tree_detailed(root2)
+print(max_data_level_wise(root2))
+print()
+print_tree_detailed(root3)
+print(max_data_level_wise(root3))
