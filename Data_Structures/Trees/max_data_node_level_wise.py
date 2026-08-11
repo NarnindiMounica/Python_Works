caecc7abc3eabbfea07847855bfd9fe13296e82e
NarnindiMounica <mounica.narnindi12@gmail.com>
2026-08-11 23:30:59 +0530

    trees: max data node level wise

--- Data_Structures/Trees/max_data_node_level_wise.py
+++ Data_Structures/Trees/max_data_node_level_wise.py
@@ -1,8 +1,18 @@
 from collections import deque
 
+from generic_trees_input import predefined_generic_trees
+
 class Tree:
     def __init__(self, val):
         self.val = val
         self.children = []
 
-    def max_data_level_wise(self):
+def max_data_level_wise(root):
+    if root == None:
+        return []
+    
+    queue = deque([root])
+    max_node_list = []
+    while queue != 0:
+        current_node
+
