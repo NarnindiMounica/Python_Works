e949ddee2062c1ee1b88c5c3e76f786134569004
NarnindiMounica <mounica.narnindi12@gmail.com>
2026-08-14 23:52:34 +0530

    binary trees: balanced binary tree function

---
+++ Data_Structures/Binary_Trees/balanced_binary_tree.py
@@ -0,0 +1,13 @@
+def is_balanced(root):
+    """
+    Function to check if a binary tree is height-balanced.
+    :param root: TreeNode -> root of the binary tree
+    :return: bool -> True if the tree is balanced, False otherwise
+    """
+    # TODO: Implement this function
+    if root == None:
+        return True
+    
+    height = 1
+    max_height = 0
+    left_height =  is_balanced(root.left) 
