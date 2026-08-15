15734053f53833570e3fb2c4df2b0f16ecf05c9b
NarnindiMounica <mounica.narnindi12@gmail.com>
2026-08-15 23:11:30 +0530

    binary trees: sum of left leaves

--- Data_Structures/Binary_Trees/sum_of_left_leaves.py
+++ Data_Structures/Binary_Trees/sum_of_left_leaves.py
@@ -1,14 +1,24 @@
 from simple_node import BinaryTreeNode, print_binary_tree_detailed
 import take_input_binary_tree
 
-root = take_input_binary_tree.take_input_for_binary_tree()
-
 def left_leaves_sum(root):
+
+    leaves_sum = 0
 
     if root == None:
         return 0
-    sum = 0
-    print(root.left.data)
+
+    if root.left != None and root.left.left == None and root.left.right == None:
+        leaves_sum = leaves_sum + root.left.data
+
+    leaves_sum += left_leaves_sum(root.left)
+    leaves_sum += left_leaves_sum(root.right)
+
+    return leaves_sum     
+
+
+root = take_input_binary_tree.take_input_for_binary_tree()
+print(left_leaves_sum(root))
 
     
 
