279ca7f18cc09ee71e10bdd17234d0d0ca3ad4a2
NarnindiMounica <mounica.narnindi12@gmail.com>
2026-08-17 22:13:20 +0530

    binary search trees: print node data within given limits

---
+++ Data_Structures/Binary_Search_Trees/print_limits_in_range.py
@@ -0,0 +1,16 @@
+from predefined_bst import create_predefined_bst
+
+def print_bst_in_range(root, low, high):
+    if root is None:
+        return None
+
+    if low < root.data:
+        print_bst_in_range(root.left, low, high)
+
+    if root.data < high:
+        print_bst_in_range(root.right, low, high)
+
+    if low <= root.data <= high:
+        print(root.data)
+
+root1,                 
