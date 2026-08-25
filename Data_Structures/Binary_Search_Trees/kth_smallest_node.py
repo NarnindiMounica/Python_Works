0c427f51e0e47503ea0004d8dd652f2d103db592
NarnindiMounica <mounica.narnindi12@gmail.com>
2026-08-26 03:49:21 +0530

    binary search trees: kth smallest node

---
+++ Data_Structures/Binary_Search_Trees/kth_smallest_node.py
@@ -0,0 +1,21 @@
+class TreeNode:
+    def __init__(self, val=0, left=None, right=None):
+        self.val = val
+        self.left = left
+        self.right = right
+
+def kth_smallest(root, k):
+    # Core logic for the learner to implement
+    
+    
+    def inorder(root, lst):
+        if root == None:
+            return None
+        inorder(root.left, lst)
+        lst.append(root.val)
+        inorder(root.right, lst)
+        
+    lst = []
+    inorder(root, lst)
+    
+    return lst[k-1]
