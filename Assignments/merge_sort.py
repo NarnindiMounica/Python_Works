0317041a0c14a824acaa398d80acc764a58ec0c0
NarnindiMounica <mounica.narnindi12@gmail.com>
2026-09-05 23:27:05 +0530

    assigments: merge sort

--- Assignments/merge_sort.py
+++ Assignments/merge_sort.py
@@ -8,5 +8,20 @@ def merge(arr, l, m, r):
 
     # Copy data to temporary arrays L[] and R[]
     for i in range(0, n1):
-    L[i] = arr[l + i]
+        L[i] = arr[l + i]
     for j in range(0, n2):
+        R[j] = arr[m + 1 + j]
+
+    # Merge the temporary arrays back into arr[l..r]
+    i = 0
+    j = 0
+    k = l
+
+    while i < n1 and j < n2:
+    if L[i] <= R[j]:
+        arr[k] = L[i]
+        i += 1
+    else:
+        arr[k] = R[j]
+        j += 1
+        k += 1
