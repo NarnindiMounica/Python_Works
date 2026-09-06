8d9bcacfb899ffe76ca7da2040472a31e9cd15a3
NarnindiMounica <mounica.narnindi12@gmail.com>
2026-09-06 23:52:46 +0530

    quick sorting

--- Assignments/quick_sort.py
+++ Assignments/quick_sort.py
@@ -10,7 +10,19 @@ def partitionFunction(l1, s, e):
         i += 1
     l1[rightPosition], l1[e] = l1[e], l1[rightPosition] 
 
-    pivotIndex = rightPosition               
+    pivotIndex = rightPosition  
+
+    #now make sure that everything smaller than pivot is on left and greater than pivot on the right
+    start = s
+    end = e
+
+    while (start<pivotIndex and end>pivotIndex):
+        if(l1[start]< pivot):
+            start += 1
+        elif(l1[end] >= pivot):
+            end -= 1
+        else:
+            l1[start], l1[end] = l1[end], l1[start]                     
 
 def quicksorting(l1, s, e):
     if (s >= e):
