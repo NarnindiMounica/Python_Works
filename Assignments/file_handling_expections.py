ce8927afbbf7b1a829c79e2185e1f20da3fea5f9
NarnindiMounica <mounica.narnindi12@gmail.com>
2026-09-13 23:39:06 +0530

    file handling with exception

--- Assignments/file_handling_expections.py
+++ Assignments/file_handling_expections.py
@@ -1,6 +1,8 @@
 try:
     with open("example.txt", "r") as file_obj:
         contents = file_obj.read()
-
+        
 except FileNotFoundError:
     print("can't read contents, file is not found")
+else:
+    print(contents)    
