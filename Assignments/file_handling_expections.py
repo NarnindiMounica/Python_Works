7c267ba514f5d01dbc9ce38eb90d2b79205c9f3d
NarnindiMounica <mounica.narnindi12@gmail.com>
2026-09-13 23:40:29 +0530

    file handling with exception

--- Assignments/file_handling_expections.py
+++ Assignments/file_handling_expections.py
@@ -6,3 +6,7 @@ except FileNotFoundError:
     print("can't read contents, file is not found")
 else:
     print(contents)    
+finally:
+    if file_obj in locals() or not file_obj.close():
+        file_obj.close()
+        print("file is closed now")
