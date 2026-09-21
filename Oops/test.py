6b22211ad24a26255b77efeb935154781904dd56
NarnindiMounica <mounica.narnindi12@gmail.com>
2026-09-21 21:30:53 +0530

    testing

--- Oops/test.py
+++ Oops/test.py
@@ -1,7 +1,16 @@
+import ctypes
 class CustomList:
     def __init__(self):
         initial_capacity = 1
         self.capacity = initial_capacity
         self.size = 0
 
-        
+    def __create_array(self, capacity):
+        return 2*capacity * (ctypes.py_object)()
+
+    def __resize_array(self, new_capacity):
+        return self.__create_array(new_capacity)
+
+    def append(self, item):
+        if self.size == self.capacity:
+            self.array = self.__resize_array()
