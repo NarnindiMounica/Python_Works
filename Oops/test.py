e9e90225614f4ae9646236fd2e4169d92cb4dd38
NarnindiMounica <mounica.narnindi12@gmail.com>
2026-09-21 21:40:09 +0530

    testing

--- Oops/test.py
+++ Oops/test.py
@@ -4,13 +4,22 @@ class CustomList:
         initial_capacity = 1
         self.capacity = initial_capacity
         self.size = 0
+        self.array = self.__create_array(self.capacity)
 
     def __create_array(self, capacity):
-        return 2*capacity * (ctypes.py_object)()
+        return (capacity * ctypes.py_object)()
 
     def __resize_array(self, new_capacity):
-        return self.__create_array(new_capacity)
+        new_array = self.__create_array(new_capacity)
+        for i in range(self.size):
+            new_array[i] = self.array[i]
+
+        self.capacity = new_capacity
+        self.array = new_array   
+
 
     def append(self, item):
         if self.size == self.capacity:
-            self.array = self.__resize_array()
+            self.array = self.__resize_array(2*self.capacity)
+
+        self.array[self.size] = item
